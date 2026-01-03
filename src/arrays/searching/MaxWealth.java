c8a9469b8fb90ad40b2d75c3cf5dc402d8625c64
AyanCBLUE <blue.ayanc@gmail.com>
2026-01-03 17:08:24 +0530

    Update README with linear search topics

---
+++ src/arrays/searching/MaxWealth.java
@@ -0,0 +1,21 @@
+package arrays.basics;
+
+public class MaxWealth {
+    public static void main(String[] args){
+        int [][] amount = {{1,6},{2,10},{10,20}};
+        System.out.println(maximumWealth(amount));
+    }
+    static int maximumWealth(int[][] amount){
+        int ans =Integer.MIN_VALUE;
+        for (int[] ints : amount) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
